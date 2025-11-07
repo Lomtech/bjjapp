a80b239e176e8ccf51aa474d7dd02e19525f8532
Lom <lom@air-von-lom.speedport.ip>
2025-11-07 02:42:16 +0100

    u

--- app.js
+++ app.js
@@ -3687,7 +3687,7 @@ function loadGoogleMapsScript() {
     }
 
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly&callback=initGoogleMaps`;
+    sscript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly&callback=initGoogleMaps`;
     script.async = true;
     script.defer = true;
     script.onerror = () =>
